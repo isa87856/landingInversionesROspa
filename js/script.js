e3a1edafacd2797c072c2bfa452300f9c3a6f0b9
Isa <torresosorio.isabel1@gmail.com>
2025-05-06 17:39:29 -0300

    pimeros inicios

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     loadComponent("components/header.html", "header-container");
     loadComponent("components/footer.html", "footer-container");
     loadComponent("components/carousel.html", "carousel-container");
+    loadComponent("components/propiedades.html", "propiedades-container");
+
   });
   
   function loadComponent(filePath, containerId) {
