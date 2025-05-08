847dfebaef05ea03f89bedfd7d862f1c6feee17f
Isa <torresosorio.isabel1@gmail.com>
2025-05-07 21:45:32 -0300

    modificaciones3

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     loadComponent("components/header.html", "header-container");
     loadComponent("components/footer.html", "footer-container");
-    loadComponent("components/carousel.html", "carousel-container");
+    /*loadComponent("components/carousel.html", "carousel-container");*/
     loadComponent("components/propiedades.html", "propiedades-container");
     loadComponent("components/aboutUs.html", "nosotros-container");
     loadComponent("components/tuPropiedad.html", "tuPropiedad-container");
+    loadComponent("components/experiencias.html", "experiencias-container");
     
 
   });
